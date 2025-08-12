fe92277cec6cc258e07b0c826d421078a0d1e85b
s.soler <s.soler@alumno.um.edu.ar>
2025-08-12 15:55:46 -0300

    Agregadas relaciones en empleado

--- tp5-gestion-empleados/src/main/java/com/um/tp5/domain/Empleado.java
+++ tp5-gestion-empleados/src/main/java/com/um/tp5/domain/Empleado.java
@@ -1,10 +1,14 @@
 package com.um.tp5.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "empleados")
@@ -34,5 +38,11 @@ public class Empleado {
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal salario;
 
-    //Falta departamento, cuando cree departamento
+    @ManyToMany
+    @JoinTable(
+            name = "empleado_proyecto",
+            joinColumns = @JoinColumn(name = "empleado_id"),
+            inverseJoinColumns = @JoinColumn(name = "proyecto_id")
+    )
+    private Set<Proyecto> proyectos = new HashSet<>();
 }
