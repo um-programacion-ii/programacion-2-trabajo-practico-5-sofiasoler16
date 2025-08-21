467eea6edc2283e51cdc5106e79361620432f521
s.soler <s.soler@alumno.um.edu.ar>
2025-08-21 18:18:43 -0300

    Agregadas consultas de Departamento

--- tp5-gestion-empleados/src/main/java/com/um/tp5/repository/DepartamentoRepository.java
+++ tp5-gestion-empleados/src/main/java/com/um/tp5/repository/DepartamentoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+    Optional<Departamento> findByNombre(String nombre);
 }
