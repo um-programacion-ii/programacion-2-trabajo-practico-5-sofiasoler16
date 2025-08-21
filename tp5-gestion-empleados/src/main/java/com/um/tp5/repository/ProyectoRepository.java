4e5c59955007c04098d2fb042039f16c333371ff
s.soler <s.soler@alumno.um.edu.ar>
2025-08-21 18:20:25 -0300

    Agregadas consultas de Proyecto

--- tp5-gestion-empleados/src/main/java/com/um/tp5/repository/ProyectoRepository.java
+++ tp5-gestion-empleados/src/main/java/com/um/tp5/repository/ProyectoRepository.java
@@ -4,5 +4,6 @@ import com.um.tp5.domain.Proyecto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {
-
+    @Query("SELECT p FROM Proyecto p WHERE p.fechaFin IS NULL OR p.fechaFin > :hoy")
+    List<Proyecto> findActivos(LocalDate hoy);
 }
