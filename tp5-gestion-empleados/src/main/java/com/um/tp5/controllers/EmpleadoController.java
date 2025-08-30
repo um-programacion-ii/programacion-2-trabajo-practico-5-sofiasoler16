fb2956386987031f8381351d97f3d96d13c52ae1
s.soler <s.soler@alumno.um.edu.ar>
2025-08-30 11:51:25 -0300

    Controller empleado

---
+++ tp5-gestion-empleados/src/main/java/com/um/tp5/controllers/EmpleadoController.java
@@ -0,0 +1,24 @@
+package com.um.tp5.controllers;
+
+import com.um.tp5.domain.Empleado;
+import com.um.tp5.service.EmpleadoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/empleados")
+@Validated
+public class EmpleadoController {
+
+    private final EmpleadoService empleadoService;
+
+    public EmpleadoController(EmpleadoService empleadoService) {
+        this.empleadoService = empleadoService;
+    }
+    
+}
