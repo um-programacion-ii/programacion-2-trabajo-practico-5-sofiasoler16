a743e26ab01d5b240a80ad8c62790f61214104b6
s.soler <s.soler@alumno.um.edu.ar>
2025-08-30 13:13:34 -0300

    Agregados endpoints de Empleado

--- tp5-gestion-empleados/src/main/java/com/um/tp5/controllers/EmpleadoController.java
+++ tp5-gestion-empleados/src/main/java/com/um/tp5/controllers/EmpleadoController.java
@@ -21,4 +21,42 @@ public class EmpleadoController {
         this.empleadoService = empleadoService;
     }
 
+    @GetMapping
+    public List<Empleado> obtenerTodos() {
+        return empleadoService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Empleado obtenerPorId(@PathVariable Long id) {
+        return empleadoService.buscarPorId(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Empleado crear(@Valid @RequestBody Empleado empleado) {
+        return empleadoService.guardar(empleado);
+    }
+
+    @PutMapping("/{id}")
+    public Empleado actualizar(@PathVariable Long id, @Valid @RequestBody Empleado empleado) {
+        return empleadoService.actualizar(id, empleado);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable Long id) {
+        empleadoService.eliminar(id);
+    }
+
+    @GetMapping("/departamento/{nombre}")
+    public List<Empleado> obtenerPorDepartamento(@PathVariable String nombre) {
+        return empleadoService.buscarPorDepartamento(nombre);
+    }
+
+    @GetMapping("/salario")
+    public List<Empleado> obtenerPorRangoSalario(
+            @RequestParam BigDecimal min,
+            @RequestParam BigDecimal max) {
+        return empleadoService.buscarPorRangoSalario(min, max);
+    }
 }
